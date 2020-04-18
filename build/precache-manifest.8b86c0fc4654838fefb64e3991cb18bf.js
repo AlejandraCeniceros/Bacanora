fffb4e22a6a5f4d67f742e3488a0d0e5fb6e6271
Alejandra Ceniceros <AlejandraCeniceros@Alejandras-MacBook-Air.local>
2020-04-17 18:24:27 -0700

    New update

---
+++ build/precache-manifest.8b86c0fc4654838fefb64e3991cb18bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfda6f591fcc0a83f6d7abdf0aed4d34",
+    "url": "/bacanora/index.html"
+  },
+  {
+    "revision": "0714c69e7e193b72919b",
+    "url": "/bacanora/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c0385987af1fc5d7defd",
+    "url": "/bacanora/static/js/2.95f0bd3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bacanora/static/js/2.95f0bd3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0714c69e7e193b72919b",
+    "url": "/bacanora/static/js/main.dfe08d40.chunk.js"
+  },
+  {
+    "revision": "f3ad70a547affcf989e8",
+    "url": "/bacanora/static/js/runtime-main.906a2f7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bacanora/static/media/logo.5d5d9eef.svg"
+  }
+]);
