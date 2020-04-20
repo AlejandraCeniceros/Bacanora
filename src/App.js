ea4e1824324497f8fe485c1d0e32164af4be3fac
Alejandra Ceniceros <AlejandraCeniceros@Alejandras-MacBook-Air.local>
2020-04-20 16:56:29 -0700

    c

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProfileImage from './img/2nd test.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 Bacanora />
+      <h1 "Bacanora" />
       <h2 by AlejandraCeniceros />
     </div>
   );
