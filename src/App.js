c650d10e93e6df7a920bfc1296732f71c3e0c6a7
Alejandra Ceniceros <AlejandraCeniceros@Alejandras-MacBook-Air.local>
2020-04-21 00:41:46 -0700

    por lo menos ya se ve

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-/*import ProfileImage from './img/2nd test.png'*/
-
 import React, {Component, Fragment} from 'react';
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Chart from 'chart.js';
+import Map from './components/Map';
 
 export default class App extends Component{
 
@@ -30,102 +28,97 @@ export default class App extends Component{
     this.setState({context: this.canvas.getContext('2d')});
   }
 
-  shouldComponentUpdate(nextProps){
-    return this.state.data !== nextProps.data;
-  }
-render() {
-     const {data} = this.state;
-     const {date, confirmed, deaths, recovered} = data;
-     
-     console.log(this.state);
+  render() {
+    const {data, arr} = this.state;
+    const {date, confirmed, deaths, recovered} = data;
+    //console.log(this.state);
 
-     const arr_cofirmed = arr.map(e =>{
-       return e.confirmed;
-     });
+    const arr_cofirmed = arr.map(e =>{
+      return e.confirmed;
+    });
 
-     const arr_dates = arr.map(e =>{
-       return e.date;
-     });
+    const arr_dates = arr.map(e =>{
+      return e.date;
+    });
 
-     const arr_deaths = arr.map(e =>{
-       return e.deaths;
-     });
+    const arr_deaths = arr.map(e =>{
+      return e.deaths;
+    });
 
-     const arr_recovereds = arr.map(e =>{
-       return e.recovered;
-     });
-
-     const myChart = new Chart(this.state.context, {
-       type: 'line',
-       data: {
-           labels: arr_dates,
-           datasets: [{
-               label: 'Casos confirmados',
-               fill: false,
-               data: arr_cofirmed,
-               backgroundColor: 'rgba(22, 10, 0, 11)',
-               borderColor: 'rgba(22, 10, 0, 11)',
-               borderWidth: 2
-           },
-           {
-             label: 'Fallecimientos',
-             fill: false,
-             data: arr_deaths,
-             backgroundColor: 'rgba(22, 10, 0, 11)',
-             borderColor: 'rgba(22, 10, 0, 11)',
-             borderWidth: 2
-         },
-         {
-           label: 'Pacientes recuperados',
-           fill: false,
-           data: arr_recovereds,
-           backgroundColor: 'rgba(22, 10, 0, 11)',
-           borderColor: 'rgba(22, 10, 0, 11)',
-           borderWidth: 2
-       }]
-       },
-       options: {
-           scales: {
-               yAxes: [{
-                   ticks: {
-                       beginAtZero: true
-                   }
-               }]
-           }
-       }
-   });
+    const arr_recovereds = arr.map(e =>{
+      return e.recovered;
+    });
+    
 
 
+    const myChart = new Chart(this.state.context, {
+      type: 'line',
+      data: {
+          labels: arr_dates,
+          datasets: [{
+              label: 'Casos confirmados (hasta el día de hoy)',
+              fill: false,
+              data: arr_cofirmed,
+              backgroundColor: 'rgba(0, 0, 255, 1)',
+              borderColor: 'rgba(0, 0, 255, 255)',
+              borderWidth: 2
+          },
+          {
+            label: 'Fallecimientos (hasta el día de hoy)',
+            fill: false,
+            data: arr_deaths,
+            backgroundColor: 'rgba(255, 0, 0, 1)',
+            borderColor: 'rgba(255, 0, 0, 255)',
+            borderWidth: 2
+        },
+        {
+          label: 'Pacientes recuperados (hasta el día de hoy)',
+          fill: false,
+          data: arr_recovereds,
+          backgroundColor: 'rgba(0, 255, 0, 1)',
+          borderColor: 'rgba(0, 255, 0, 255)',
+          borderWidth: 2
+      }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
 
+    return (
+       <Fragment>
 
-
-
-     /* */
-     return (
-        <Fragment>
-
-           <div className="container">
-           <h1>Covid19 México</h1>
-           <div className="row">
-
-           <div className="col text-center">
+          <div className="container">
+          <h1>Covid19 México</h1>
+          <div className="row">
+            
+              <div className="col text-center">
                 <div className="display-1">{date}</div>
-                <div className="text-secondary"><strong>Ultima actualización</strong>/strong></div>
+                <div className="text-secondary"><strong>Fecha del último reporte</strong></div>
                 <div className="display-1">{confirmed}</div>
-                <div className="text-secondary"><strong>Confirmados</strong></div>
+                <div className="text-secondary"><strong>Casos confirmados</strong></div>
                 <div className="display-1">{deaths}</div>
                 <div className="text-secondary"><strong>Fallecimientos</strong></div>
                 <div className="display-1">{recovered}</div>
-                <div className="text-secondary"><strong>Recuperados</strong></div>
+                <div className="text-secondary"><strong>Pacientes recuperados</strong></div>
                 <br/>
                 <canvas id="myChart" ref={c => this.canvas = c} width="400" height="400"></canvas>
-                 <br/>
+                <br/>
+                <h1>Covid19 en el mundo</h1>
+                <Map/>
+                <br/>
                 <footer>Datos tomados de "The Center for Systems Science and Engineering (CSSE) at JHU", public api: https://github.com/pomber/covid19</footer>
               </div>
             </div>
-
-           </div>
-        </Fragment>
-     );
-   }
- }
+  
+          </div>
+       </Fragment>
+    );
+  }
+}
---
+++ src/components/Map.js
@@ -0,0 +1,123 @@
+import React, {Component, useRef, createRef} from 'react';
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import './map.scss'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlamFuZHJhY2VuaWNlcm9zIiwiYSI6ImNrOTlreDV3dTF0ZHUzZXBnZHQzMjRqNjQifQ.M39Iy0yeNbDNtIFBXZBoUw';
+
+export default class Map extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.mapboxElRef = createRef();
+
+        this.state = {
+            lng: -102.10232,
+            lat: 23.68367,
+            zoom: 3.60,
+        }
+    }
+
+    componentDidMount(){
+        const url = 'https://corona.lmao.ninja/v2/jhucsse';
+
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [this.state.lng, this.state.lat],
+            zoom: this.state.zoom
+        });
+
+        fetch(url).then(r => r.json()).then(data => {
+            
+            const points = data.map((point, index) =>{
+                //console.log(country);
+                const {stats, coordinates, province, country} = point;
+                const {latitude, longitude} = coordinates;
+                const {confirmed, deaths} = stats;
+                return {
+                    type: "Feature",
+                    geometry: {
+                        type: "Point",
+                        coordinates: [
+                            longitude,
+                            latitude
+                        ]
+                    },
+                    properties: {
+                        id: index, // unique identifier in this case the index
+                        country: country,
+                        province: province,
+                        cases: confirmed,
+                        deaths: deaths
+                    }
+                } 
+            });
+
+            map.once('load', ()=>{
+                map.addSource("points", {
+                    type: "geojson",
+                    data: {
+                      type: "FeatureCollection",
+                      features: points
+                    }
+                });
+
+                map.addLayer({
+                    id: "circles",
+                    source: "points", // this should be the id of the source
+                    type: "circle",
+                    // paint properties
+                    paint: {
+                        "circle-opacity": 0.75,
+                        "circle-stroke-width": 1,
+                        "circle-stroke-width": [
+                            "interpolate",
+                            ["linear"],
+                            ["get", "cases"],
+                            1, 1,
+                            100000, 1.75,
+                        ],
+                        "circle-radius": 4,
+                        "circle-color": "#FFEB3B",
+                        "circle-color": [
+                            "interpolate",
+                            ["linear"],
+                            ["get", "cases"],
+                            1, '#ffffb2',
+                            5000, '#fed976',
+                            10000, '#feb24c',
+                            25000, '#fd8d3c',
+                            50000, '#fc4e2a',
+                            75000, '#e31a1c',
+                            100000, '#b10026'
+                        ],
+                        "circle-radius": [
+                            "interpolate",
+                            ["linear"],
+                            ["get", "cases"],
+                            1, 4,
+                            1000, 8,
+                            4000, 10,
+                            8000, 14,
+                            12000, 18,
+                            100000, 40
+                        ]
+                    }
+                });
+            });
+
+            //console.log(points);
+        });
+    }
+
+    render() {
+
+        return (
+             <div className="mapContainer">
+                <div className="mapBox" ref={e => this.mapContainer  = e} />
+            </div>
+        );
+    }
+}
