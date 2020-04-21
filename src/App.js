310e5874e36d0cbd97c7f9c1c3367515d78a6774
Alejandra Ceniceros <AlejandraCeniceros@Alejandras-MacBook-Air.local>
2020-04-20 22:39:52 -0700

    textura 3d xd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
-import React from 'react';
-import ProfileImage from './img/2nd test.png'
+/*import ProfileImage from './img/2nd test.png'*/
+
+import React, {Component, Fragment} from 'react';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Chart from 'chart.js';
 
-function App() {
-  const newLocal = "Bacanora";
-  return (
-    <div className="App">
-      <h1 newLocal />
-      <h2 by AlejandraCeniceros />
-    </div>
-  );
-}
+export default class App extends Component{
 
-export default App;
+  constructor(props){
+    super(props)
+
+    this.state = {
+      url: 'https://pomber.github.io/covid19/timeseries.json',
+      data: {},
+      context: '',
+      arr: []
+    }
+    this.canvas = React.createRef();
+
+  }
+
+  componentDidMount() {
+    const {url} = this.state;
+    fetch(url).then(res => res.json()).then(json => this.setState({
+      data: json.Mexico[json.Mexico.length - 1],
+      arr: json.Mexico
+    }));
+    this.setState({context: this.canvas.getContext('2d')});
+  }
+
+  shouldComponentUpdate(nextProps){
+    return this.state.data !== nextProps.data;
+  }
+render() {
+     const {data} = this.state;
+     const {date, confirmed, deaths, recovered} = data;
+     console.log(this.state.data);
+     return (
+        <Fragment>
+
+           <div className="container">
+           <h1>Covid19 México</h1>
+           <div className="row">
+
+           <div className="col text-center">
+                <div className="display-1">{date}</div>
+                <div className="text-secondary"><strong>Ultima actualización</strong>/strong></div>
+                <div className="display-1">{confirmed}</div>
+                <div className="text-secondary"><strong>Confirmados</strong></div>
+                <div className="display-1">{deaths}</div>
+                <div className="text-secondary"><strong>Fallecimientos</strong></div>
+                <div className="display-1">{recovered}</div>
+                <div className="text-secondary"><strong>Recuperados</strong></div>
+                <br/>
+                <canvas id="myChart" ref={c => this.canvas = c} width="400" height="400"></canvas>
+                <br/>
+                <footer>Datos tomados de "The Center for Systems Science and Engineering (CSSE) at JHU", public api: https://github.com/pomber/covid19</footer>
+              </div>
+            </div>
+
+           </div>
+        </Fragment>
+     );
+   }
+ }
